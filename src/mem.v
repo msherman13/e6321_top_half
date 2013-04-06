43c0d57ece966c84a1927e7b81f4ee02e7c26e75
kw2500 <kw2500@columbia.edu>
2013-04-05 20:20:18 -0400

    Added inputs to RAM from module

--- src/mem.v
+++ src/mem.v
@@ -1,17 +1,20 @@
 module d_mem(input		clk, write_en,
 	     input[5:0]		addr,
-	     input[31:0]	write_data,
-	     output[31:0]	read_data
+	     inout[31:0]	data
 	);
 
 	//data memory
 
 	reg[31:0] RAM[63:0];
+	reg[31:0] data_out;
 	
-	assign read_data = RAM[addr];  // word aligned
+		// Tri-State Buffer control
+	assign data = ~write_en? data_out : 'bz;
+	
+	data_out = RAM[addr];  // word aligned
 
 	always @(posedge clk)
-		if(write_en) RAM[addr] = write_data;
+		if(write_en) RAM[addr] = data;
 endmodule
 
 module i_mem(input[5:0]		addr,
--- src/mipstop.v
+++ src/mipstop.v
@@ -1,6 +1,7 @@
 module mipstop
 (
-	input clk, reset, accdone, 
+	input clk, reset, accdone, writedata,
+	input [31:0] datain,
 	output memwrite,
 	output [31:0] dataaddr,
 	output [31:0] writedata,
@@ -10,6 +11,11 @@ module mipstop
 );
 	wire [31:0] instr, readdata, pc;
 	mipspipelined mips(clk, reset, accdone, instr, readdata, memwrite, pc, dataaddr, writedata, accbypassA, startaddrA, datasizeA);
-	d_mem dm(clk, memwrite, dataaddr[7:2], writedata, readdata);
+	d_mem dm(clk, memwrite, dataaddr[7:2],data);
 	i_mem im(pc[7:2], instr);
+	
+	
+	// Tri-State Buffer control
+	assign readdata = ~memwrite? data : 'bz;
+	assign data = memwrite? writedata : (writedata? datain: 'bz);
 endmodule
